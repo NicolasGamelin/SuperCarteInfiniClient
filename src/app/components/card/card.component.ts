24ea5b18c3a7d150272d4899088980e858dae479
Nicolas Gamelin <2259825@cegepmontpetit.ca>
2024-02-17 14:25:52 -0500

    REFACTOR service au lieu de méthode dans le component
    Je n'avais pas vu que les méthodes étaient dans le service. Les components utilisent maintenent ces méthodes.

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -2,6 +2,8 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Card } from 'src/app/models/models';
 import {HttpClient} from "@angular/common/http";
 import {lastValueFrom} from "rxjs";
+import {ApiService} from "../../services/api.service";
+
 
 @Component({
   selector: 'app-card',
@@ -16,18 +18,14 @@ export class CardComponent implements OnInit {
   @Input() health:number = 0;
   beautifulBackUrl = "https://i.pinimg.com/236x/3c/73/0d/3c730d6df70700a3c912a3c87d6d2027.jpg";
 
-  constructor(public http:HttpClient) {
+  constructor(public http:HttpClient, public api:ApiService) {
 
   }
 
-  ngOnInit() {
-    this.getPlayerCards();
+  async ngOnInit() {
+    this.cards = await this.api.getPlayersCards();
   }
 
-  async getPlayerCards(){
-    let res = await lastValueFrom(this.http.get<Card[]>('http://localhost:5057/api/Card/GetPlayersCards'));
-    this.cards = res;
-    console.log(res);
-  }
+
 
 }
--- src/app/components/store/store.component.ts
+++ src/app/components/store/store.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import {Card} from "../../models/models";
 import {HttpClient} from "@angular/common/http";
 import {lastValueFrom} from "rxjs";
+import {ApiService} from "../../services/api.service";
 
 @Component({
   selector: 'app-store',
@@ -16,16 +17,12 @@ export class StoreComponent implements OnInit{
   @Input() health:number = 0;
   beautifulBackUrl = "https://i.pinimg.com/236x/3c/73/0d/3c730d6df70700a3c912a3c87d6d2027.jpg";
 
-  constructor(public http:HttpClient) { }
+  constructor(public http:HttpClient, public api:ApiService) { }
 
-  ngOnInit() {
-    this.getPlayerCards();
+ async ngOnInit() {
+    this.cards = await this.api.getAllCards();
   }
 
 
-  async getPlayerCards(){
-    let res = await lastValueFrom(this.http.get<Card[]>('http://localhost:5057/api/Card/GetAllCards'));
-    this.cards = res;
-    console.log(res);
-  }
+
 }
