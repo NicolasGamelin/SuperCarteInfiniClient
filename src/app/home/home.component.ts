9a7ffa7453c1ced5efca964cda726806aade580b
Nicolas Gamelin <2259825@cegepmontpetit.ca>
2024-02-19 16:12:56 -0500

    FCT : SearchIndex
    Ajout d'un index de recherche. Annuler la recherche, puis la rejoindre ne risque plus de nous faire rejoindre une partie sur le compte de la méthode lancer ultérieurement.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,6 +13,7 @@ export class HomeComponent implements OnInit {
   constructor(public router: Router, public match: MatchService) { }
 
   searching:boolean = false;
+  searchingIndex:number = -1;
   ngOnInit() {
 
   }
@@ -22,9 +23,11 @@ export class HomeComponent implements OnInit {
   async joinMatch() {
 
     this.searching = true;
+    this.searchingIndex++;
+    let i = this.searchingIndex;
     await this.delay(30000);
     //prob pas tres bon
-    if(this.searching){
+    if(this.searching && i == this.searchingIndex){
       let matchId = -1;
      await this.router.navigate(['/match/' + matchId]);
       this.searching = false;
