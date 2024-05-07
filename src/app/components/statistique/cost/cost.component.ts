be8ad77d2ea892d983b2a7ed3491ae861f2aca7f
zk <2243151@cegepmontpetit.ca>
2024-05-07 15:49:18 -0400

    FCT COÛT
    afficher les statistique des cartes par leur coût

--- src/app/components/statistique/cost/cost.component.ts
+++ src/app/components/statistique/cost/cost.component.ts
@@ -1,11 +1,78 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {Deck} from "../../../models/models";
+import {ApiService} from "../../../services/api.service";
 
 @Component({
   selector: 'app-cost',
   templateUrl: './cost.component.html',
   styleUrls: ['./cost.component.css']
 })
-export class CostComponent {
+export class CostComponent implements OnInit, OnChanges{
   @Input()  selectedDeckID:number = 0;
+  chart : any;
+  StatByCost: any[] = [];
+  d: {cost: string, y: string}[] = [];
+  constructor(public  Service:ApiService) {
+  }
+
+
+
+
+  ngOnChanges(changes: SimpleChanges) {
+
+    this.updateChart();
+    console.log(this.d);
+  }
+
+  ngOnInit() {
+
+    this.updateChart();
+    console.log(this.d);
+  }
+
+
+  getChartInstance(chart: object) {
+    this.chart = chart;
+    this.updateChart();
+  }
+  chartOptions = {
+    title:{
+      text: "stat par coût"
+    },
+    animationEnabled: true,
+    axisY: {
+      includeZero: true,
+      title: "carte",
+interval: 1
+    },
+    axisX:{ includeZero: true,
+      title: "count",
+      interval: 1},
+
+    data: [{
+      type: "bar",
+      indexLabel: "{y}",
+      yValueFormatString: "#",
+      dataPoints: [
+        {}
+      ]
+    }]
+  }
+
+
+
+
+  async updateChart() {
+
+
+    this.StatByCost = await this.Service.StatByCost(this.selectedDeckID);
+    this.d = [];
+    this.StatByCost.forEach(e => {
+      this.d.push({cost: e.coût, y: e.count});
+    });
+
+    this.chartOptions.data[0].dataPoints = this.d;
+    this.chart.render();
+
+  }
 }
